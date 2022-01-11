9df369c7cd391014d289af25431f57502a381c0b
Simon Liu <sarahbradl3y@gmail.com>
2022-01-11 13:28:56 +1100

    edited graphing code

--- OurCodes/testing/graph_battery.py
+++ OurCodes/testing/graph_battery.py
@@ -1,17 +1,18 @@
 from matplotlib import pyplot as plt
 import pandas as pd
+import numpy as np
 
 time_dataE7 = pd.read_csv("batteryE7.csv", usecols = ['time.ms'])
 volt_dataE7 = pd.read_csv("batteryE7.csv", usecols = ['pm.vbat'])
-plt.plot(time_dataE7, volt_dataE7, color='pink')
+# plt.plot(time_dataE7/1000, volt_dataE7, color='pink')
 
 time_dataE5 = pd.read_csv("battery1.csv", usecols = ['time.ms'])
 volt_dataE5 = pd.read_csv("battery1.csv", usecols = ['pm.vbat'])
-plt.plot(time_dataE5, volt_dataE5, color='green')
+# plt.plot(time_dataE5/1000, volt_dataE5, color='green')
 
 time_dataE3 = pd.read_csv("batteryE3.csv", usecols = ['time.ms'])
 volt_dataE3 = pd.read_csv("batteryE3.csv", usecols = ['pm.vbat'])
-plt.plot(time_dataE3, volt_dataE3, color='red')
+# plt.plot(time_dataE3/1000, volt_dataE3, color='red')
 
 # LINE OF BEST FIT
 time_data = pd.read_csv("batteryE7.csv", usecols = ['time.ms'])
@@ -26,10 +27,13 @@ volt_data += pd.read_csv("batteryE3.csv", usecols = ['pm.vbat'])
 
 # X_values = time_data
 # Y_values = [0, 1, 4, 9, 16]
-plt.plot(time_data/3, volt_data/3, color='blue')
-# plt.plot(time_data1, volt_data1, color='red')
-plt.xlabel("Time in ms")
+plt.plot(time_data/3000, volt_data/3, color='blue')
+plt.xlabel("Time in Seconds")
 plt.ylabel("Battery") 
+
+# How many seconds would it take to fully charge:
+# First find the number of seconds:
+# np.interp(3.7, time_data, volt_data)
 
 plt.show()
 
