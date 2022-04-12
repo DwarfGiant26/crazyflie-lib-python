cda6db1106562e109b51e75ae31350c556148a99
albertusalvinjanitra@gmail.com <albertusalvinjanitra@gmail.com>
2022-04-12 14:46:45 +1000

    add 2 visualizations

--- OurCodes/flying/graph_all_xyz.py
+++ OurCodes/flying/graph_all_xyz.py
@@ -8,13 +8,11 @@ root = "new_1Intermediate"
 for path_no in os.listdir(root):
     for wind_speed in os.listdir(os.path.join(root,path_no)):
         for wind_angle in os.listdir(os.path.join(root,path_no,wind_speed)):
-            for axis in ["x","y","z"]:
+            for column in ["pm.vbat"]:
                 for drone in os.listdir(os.path.join(root,path_no,wind_speed,wind_angle)):
                     csv_path = os.path.join(root,path_no,wind_speed,wind_angle,drone)
                     data = pd.read_csv(csv_path)
-                    plt.plot(data["time.ms"],data[f"stateEstimate.{axis}"])
-                plt.xlabel(os.path.join(root,path_no,wind_speed,wind_angle)+axis)
-                plt.vlines(x = 6000,ymax=3,ymin=-3)
-                print(os.path.join(root,path_no,wind_speed,wind_angle)+axis)
+                    plt.plot(data["time.ms"],data[column])
+                plt.xlabel(os.path.join(root,path_no,wind_speed,wind_angle)+f" {column}")
                 plt.show()
 
